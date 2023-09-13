2bbd472126949b31086457dbb394de56657e8b79
Алиса Болгарина <bolgarina.a@yandex.ru>
2023-09-13 23:51:16 +0300

    8 try

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
 # os.getenv('ALLOWED_HOSTS').split(',')
 # try
