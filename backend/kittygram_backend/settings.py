c28f36f90a29b42fee2c905eea87f349f24dd5fc
Алиса Болгарина <bolgarina.a@yandex.ru>
2023-09-14 00:51:52 +0300

    trytrytry

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,14 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['51.250.17.90', '127.0.0.1', 'localhost', 'alicehw.sytes.net']
-
-# os.getenv('ALLOWED_HOSTS').split(',')
-# try
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
