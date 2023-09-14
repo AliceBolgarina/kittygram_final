9b1a97909cc85857d71b14130861ff7b76b2e1a5
Алиса Болгарина <bolgarina.a@yandex.ru>
2023-09-14 19:24:43 +0300

    cry3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# cry
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
